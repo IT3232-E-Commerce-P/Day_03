81eeed9ba7c65e206b631664898b82c1bfe961b1
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-03-30 10:37:25 +0530

    update

--- MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -1,10 +1,14 @@
 package lk.ac.vau.fas.ict.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lk.ac.vau.fas.ict.model.Student;
+
 @RestController
 @RequestMapping("/app")
 public class AppController {
@@ -17,10 +21,18 @@ public class AppController {
 	Student James = new Student("2020ICT02","James Bond",24,"IT",3.1);
 	Student Donald = new Student("2020ICT03","Donald Trump",25,"IT",3.2);
 	
+	List<Student> students = new ArrayList<Student>();
+	
     @GetMapping("/Student")
     public Student getStudent() {
     	return Bob;
     }
-	
+	@GetMapping("/Students")
+	public List<Student> getStudents(){
+		students.add(Bob);
+		students.add(James);
+		students.add(Donald);
+		return students;
+	}
 }
 
