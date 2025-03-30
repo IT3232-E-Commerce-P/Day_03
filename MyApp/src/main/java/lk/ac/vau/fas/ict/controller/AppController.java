34d7b3492d68aaa5a7b5ae29a20bb4ffee42ca80
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-03-30 14:15:42 +0530

    update

--- MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -3,8 +3,12 @@ package lk.ac.vau.fas.ict.controller;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,5 +61,40 @@ public class AppController {
             }
         }
         return filteredStudents;
+    }
+    
+    @GetMapping("/Students/sorted")
+    public List<Student> getStudentsSortedByGPA() {
+    List<Student> sortedStudents = new ArrayList<>(students);
+     for(int i=0;i<sortedStudents.size();i++) {
+    	 for(int j=i+1;j<sortedStudents.size();j++) {
+    		 if(sortedStudents.get(i).getGpa()<sortedStudents.get(j).getGpa()) {
+    			 Student temp = sortedStudents.get(i);
+    			 sortedStudents.set(i, sortedStudents.get(j));
+    			 sortedStudents.set(j, temp);
+    		 }
+    	 }
+     }
+     return sortedStudents;
+}
+    @PostMapping("/Students")
+    public String addStudent(@RequestBody Student student){
+    	students.add(student);
+    	return "Student added successfully!";
+    }
+    
+    @PutMapping("/Students/{id}")
+    public String updateStudent(@PathVariable("id")String regNo,@RequestBody Student updatedStudent) {
+    	for(Student student : students) {
+    		if(student.getRegNo().equals(regNo)) {
+    			student.setName(updatedStudent.getName());
+    			student.setAge(updatedStudent.getAge());
+    			student.setCourse(updatedStudent.getCourse());
+    			student.setGpa(updatedStudent.getGpa());
+    			return "student updated successfully!";
+    			
+    		}
+    	}
+    	return "Student not found!";
     }
 }
