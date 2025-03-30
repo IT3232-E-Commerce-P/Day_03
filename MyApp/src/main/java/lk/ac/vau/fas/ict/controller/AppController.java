512ec1de9ef661d7b50f4d78ff18d75a7c47df52
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-03-30 10:29:49 +0530

    update

--- MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -4,15 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lk.ac.vau.fas.ict.model.Student;
 @RestController
 @RequestMapping("/app")
 public class AppController {
-	@GetMapping("/msg")
-	public String myMessage() {
-		return "Hello SpringBoot";
+	@GetMapping("/home")
+	public String homeMessage() {
+		return "Welcome to Student Management System";
 	}
-	@GetMapping("/name")
-	public String myName() {
-		return "My name is SpringBoot";
-	}
+	
+	Student Bob = new Student("2020ICT01","Bob Marely",25,"IT",3.3);
+	Student James = new Student("2020ICT02","James Bond",24,"IT",3.1);
+	Student Donald = new Student("2020ICT03","Donald Trump",25,"IT",3.2);
+	
+    @GetMapping("/Student")
+    public Student getStudent() {
+    	return Bob;
+    }
+	
 }
+
